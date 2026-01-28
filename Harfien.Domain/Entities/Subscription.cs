ef764fa43f3d5a430c058c77e2b800a97102c78d
Mohamed Yousif <mohamedyousif44704@gmail.com>
2026-01-28 16:53:53 +0300

    Add Subscription and Notification entities

---
+++ Harfien.Domain/Entities/Subscription.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harfien.Domain.Entities
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public int SubscriptionPlanId { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public string Status { get; set; }
+
+        public User User { get; set; }
+        public SubscriptionPlan SubscriptionPlan { get; set; }
+    }
+}
