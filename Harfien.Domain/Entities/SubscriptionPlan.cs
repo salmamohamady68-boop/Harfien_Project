f6ade95af3eb2af80bca11a0f78bcc2ca028cedc
salmamohamady68-boop <salmamohamady68@gmail.com>
2026-01-29 00:15:15 +0200

    Add Entities

---
+++ Harfien.Domain/Entities/SubscriptionPlan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harfien.Domain.Entities
+{
+    public class SubscriptionPlan
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+       
+        public decimal Price { get; set; }
+        public int DurationInDays { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public ICollection<SubscriptionPlanDetails> SubscriptionPlanDetails { get; set; }
+
+    }
+}
