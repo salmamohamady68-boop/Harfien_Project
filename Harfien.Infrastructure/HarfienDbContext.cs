1e8a44325f6a5a37dd124f94b0eb77e4f57d2f09
Anas Alamir <anasalamir06@gmail.com>
2026-02-03 14:40:17 +0200

    fixed the missing from seeding city and area (git issues no more)

--- Harfien.Infrastructure/HarfienDbContext.cs
+++ Harfien.Infrastructure/HarfienDbContext.cs
@@ -75,7 +75,25 @@ namespace Harfien.DataAccess
                   .WithMany(u => u.SentMessages)
                   .HasForeignKey(m => m.SenderId);
 
-             
+
+                builder.Entity<City>().HasData(
+                    new City { Id = 1, Name = "Cairo" },
+                    new City { Id = 2, Name = "Giza" },
+                    new City { Id = 3, Name = "Alexanderia" },
+                    new City { Id = 4, Name = "Aswan" }
+                    //,
+                    //new City { Id = 5, Name = "Riyadh" } //sample for another country, does it need country table?
+                    );
+                builder.Entity<Area>().HasData(
+                    new Area { Id = 1, CityId = 1, Name = "Maadi" },
+                    new Area { Id = 2, CityId = 1, Name = "Helwan" },
+                    new Area { Id = 3, CityId = 1, Name = "Ramses" },
+                    new Area { Id = 4, CityId = 2, Name = "El Omarania" },
+                    new Area { Id = 5, CityId = 3, Name = "Naga Elarab" }
+                    //,
+                    //new Area { Id = 6, CityId = 5, Name ="Olaya" }
+                    );
+
         }
     }
     }
