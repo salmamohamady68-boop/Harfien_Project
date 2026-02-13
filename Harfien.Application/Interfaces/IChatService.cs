efa961be6775981b48a107fb69aca0e3adf47607
karen <karen.fakher5@gmail.com>
2026-02-13 20:04:12 +0200

    AddchatDto,AddchatmessageDto

---
+++ Harfien.Application/Interfaces/IChatService.cs
@@ -0,0 +1,16 @@
+﻿using Harfien.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harfien.Application.Interfaces
+{
+  
+    public interface IChatService
+    {
+        Task<int> CreateChatAsync(AddChatDto dto, string userId);
+        Task<object> GetByOrderIdAsync(int orderId);
+    }
+}
