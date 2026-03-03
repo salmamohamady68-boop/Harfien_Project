5079b00c54760a046c1ce1e8d9197c6b4d983a97
salmamohamady68-boop <salmamohamady68@gmail.com>
2026-03-03 21:59:29 +0200

    update order

--- Harfien.Application/DTO/Order/OrderResponseDto.cs
+++ Harfien.Application/DTO/Order/OrderResponseDto.cs
@@ -9,11 +9,13 @@ namespace Harfien.Application.DTO.Order
         public string ServiceName { get; set; } = null!;
         public string ClientName { get; set; } = null!;
         public string CraftsmanName { get; set; } = null!;
+        public string Description { get; set; }
         public decimal Amount { get; set; }
 
         public int? OrderId { get; set; }      
 
         public OrderStatus Status { get; set; }
+
         public DateTime ScheduledAt { get; set; }
         public int CraftsmanId { get; set; }
         public int ClientId { get; set; }
